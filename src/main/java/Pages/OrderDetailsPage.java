85b3196cddf08737994d53ab9bd3465b47a7df6c
Abdelrahman El Azazy <66336159+AbdelrahmanElazazy1@users.noreply.github.com>
2022-12-23 14:49:49 +0200

    Enhancements

---
+++ src/main/java/Pages/OrderDetailsPage.java
@@ -0,0 +1,13 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class OrderDetailsPage extends PageBase{
+
+	public OrderDetailsPage(WebDriver driver) {
+		super(driver);
+	}
+
+	
+	
+}
